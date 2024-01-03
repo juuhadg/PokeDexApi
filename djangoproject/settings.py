64c8f936ac4e443908408af26aa8f786bd301879
juuhadg <juku.jj@gmail.com>
2024-01-03 13:15:10 -0300

    allowed hosts

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-e$((mmkwvvi!2(4+-87rnxck*(5epsbly1of-!!$fn5l#wyw6-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['http://127.0.0.1:8000',]
 
 
 # Application definition
